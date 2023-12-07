af85563f8c624176287b0f10c7682a66fc823e85
allthepreaching <nickbetti@gmail.com>
2023-12-06 20:58:02 -0700

    Update

--- build/index.php
+++ build/index.php
@@ -4,7 +4,6 @@
 <div class="flex flex-col items-center justify-start fixed top-16 w-[calc(100vw-289px)] h-full bg-black overflow-auto whitespace-normal translate-x-72">
 
     <!-- Tags Bar -->
-    <div class="text-white h-10 w-full bg-black flex flex-row items-center justify-start">
-        tags bar
+    <div class="tags-bar-container">
     </div>
 </div>
